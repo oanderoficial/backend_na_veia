5dbd7d903f9a44177490a4da042493158ac9949c
Anderson B Silva (Ander) <32654298+oanderoficial@users.noreply.github.com>
2025-11-18 19:58:18 -0300

    update de novos conteúdos

---
+++ 4. Linguagens de Programação/Python/1. Python (Lógica de programação básica)/tipo_str.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Anderson B Silva')
+print(1, 'Anderson "B Silva"')
+
+# Aspas duplas
+print("Anderson B Silva")
+print(2, "Anderson 'B Silva'")
+
+# Escape
+print("Anderson \"B Silva\"")
+
+# r
+print(r"Anderson \"B Silva\"")
