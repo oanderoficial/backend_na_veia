aded7753053e9aa0b8c31fc5db628281e9ddad47
Anderson B Silva (Ander) <32654298+oanderoficial@users.noreply.github.com>
2025-11-21 18:08:57 -0300

    adicionando novos conteudos

---
+++ 4. Linguagens de Programação/Python/1. Python (Lógica de programação básica)/funcao_input.py
@@ -0,0 +1,17 @@
+nome = input("Digite seu nome: ")
+print(f"Olá, {nome}!")
+
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+print (f'Você tem {idade} anos, e possuí {altura} de altura.')
+
+# 1) Lendo dois números e somando
+
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+print(f"A soma é: {a + b}")
+
+# 2) Pergunta simples
+
+cor = input("Qual sua cor favorita? ")
+print("Você escolheu:", cor)
