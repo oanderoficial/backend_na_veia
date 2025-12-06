8000787341ef34c6bb5b183fdd0f8a7e7992eeff
Anderson B Silva (Ander) <32654298+oanderoficial@users.noreply.github.com>
2025-12-05 21:32:17 -0300

    add

---
+++ 4. Linguagens de Programação/Python/1. Python (Lógica de programação básica)/operadores_de_comparacao.py
@@ -0,0 +1,19 @@
+"""
+| Operador | Significado    | Exemplo  |
+| -------- | -------------- | -------- |
+| `==`     | igual a        | `a == b` |
+| `!=`     | diferente de   | `a != b` |
+| `>`      | maior que      | `a > b`  |
+| `<`      | menor que      | `a < b`  |
+| `>=`     | maior ou igual | `a >= b` |
+| `<=`     | menor ou igual | `a <= b` |
+
+"""
+
+a = 10
+b = 5
+
+print(a > b)   # True
+print(a == b)  # False
+print(a <= 10) # True
+print(a != 7)  # True
